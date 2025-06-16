cb72f38a3a169cd19099d2c94a401d5c8ec1d0bc
TMS-JSEROY\jseroy <jeassonseroy4@gmail.com>
2025-06-16 16:30:58 +0800

    For demo

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace TNMPSmokeTestingAutomation
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
---
+++ Utilities/TestResult.cs
@@ -0,0 +1,9 @@
+namespace TMNPSmokeTestingAutomation // IMPORTANT: Use your actual project's namespace
+{
+    public class TestResult
+    {
+        public string name { get; set; }
+        public bool iSSuccessful { get; set; }
+        public string failureReason { get; set; }
+    }
+}
